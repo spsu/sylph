abece0bbb9970ae2c9e0e97801adaa58685cce35
Brandon Thomas Suit <brandon.suit@gmail.com>
2010-07-05 07:23:21 -0400

    Large refactor of http module

--- utils/http.py
+++ utils/http.py
@@ -12,83 +12,33 @@ pathetic at this point.) Sylph requests are built upon this at a higher
 level. See 'comms.py'.
 """
 
-class Request(object):
-	"""Perform a synchronous HTTP request.
-
-		r = Request('http://slashdot.org/')
-		result = r.download()
-		print result.get_node_type()
+class Message(object):
+	"""A message object is a store for basic information sent over HTTP
+	This represents both Request and Response, as it's basically the
+	same information store as accessed by both endpoints.
 	"""
 
-	def __init__(self, uri, get={}, post={}, headers={}, timeout=10,
-					response_class=None):
+	# TODO: GET isn't supported: It won't be stripped from URL or placed there!
+	def __init__(self, uri='', get={}, post={}, headers={}, body=''):
 		self.uri = uri
 		self.get = get
 		self.post = post
-		self.timeout = timeout
-		self.headers = {
-			'Accept': 'text/html,application/xhtml+xml,' + \
-						'application/xml;q=0.9,*/*;q=0.8',
-			# User agent and protocol
-			'User-Agent': '%s (v%s/+http://github.com/echelon/sylph)' % \
-										(settings.SOFTWARE_NAME,
-										settings.SOFTWARE_VERSION),
-			'X-Sylph-Protocol-Version': settings.PROTOCOL_VERSION,
-		}
+		self.headers = headers
+		self.body = body
 
-		# Add custom headers and/or overwrite defaults above
-		for hk, hv in headers.iteritems():
-			self.headers[hk] = hv
+	# ============= URI/Body ==============================
 
-		self.response_class = response_class
+	def get_uri(self):
+		return self.uri
 
-	# ============= Download ==============================
+	def set_uri(self, uri):
+		self.uri = uri
 
-	def send(self, method='GET'):
-		"""Perform the download.
-		This is a synchronous request."""
-		if self.post or method.upper() == 'POST':
-			method = 'POST'
-			post = urlencode(self.post)
-			self.headers['Content-type'] = 'application/x-www-form-urlencoded'
+	def get_body(self):
+		return self.body
 
-		headers = {}
-		status = 0
-		body = ''
-
-		try:
-			uri = urlparse(self.uri)
-			conn = httplib.HTTPConnection(uri.hostname, uri.port,
-											timeout=self.timeout)
-			if method == 'POST':
-				conn.request(method, uri.path, params, self.headers)
-			else:
-				conn.request(method, uri.path, headers=self.headers)
-
-			response = conn.getresponse()
-
-			headers = {}
-			for h in response.getheaders():
-				headers[h[0]] = h[1]
-
-			# XXX: Temporary debugging...
-			status = response.status
-			if response.status != 200:
-				from sylph.utils.debug import parse_endpoint_trace
-				print parse_endpoint_trace(response.read()) # XXX Debug trace
-				raise Exception, "An exception occurred in comms."
-
-
-			body = response.read()
-			conn.close() # XXX/TODO: In the future, keep this open. 
-
-		except:
-			print "CONNECTION ERROR"
-			pass
-
-		if not self.response_class:
-			return Response.from_data(self.uri, headers, status, body)
-		return self.response_class.from_data(self.uri, headers, status, body)
+	def set_body(self, body):
+		self.body = body
 
 	# ============= HTTP Headers ==========================
 
@@ -96,14 +46,12 @@ class Request(object):
 		self.headers[key] = value
 
 	def add_headers(self, headers):
-		"""Add a dictionary of headers. Overrides previous keys."""
 		if type(headers) != dict:
 			return
 		for k, v in headers.iteritems():
 			self.headers[k] = v
 
 	def set_headers(self, headers):
-		"""Set headers. Overrides all previous headers."""
 		if type(headers) != dict:
 			return
 		self.headers = headers
@@ -145,53 +93,81 @@ class Request(object):
 	def get_post(self):
 		return self.post
 
-	# ============= Misc Accessors/Mutators ===============
+# ============ Subtypes =========================
 
-	def get_uri(self):
-		return self.uri
+class Request(Message):
+	pass
 
-	def set_uri(self, uri):
-		self.uri = uri
+class Response(Message):
+	pass
 
-	def set_response_class(self, cls):
-		self.response_class = cls
+# ============ Communication Functions ====================
 
-# ================= Server Response =======================
+def get(uri, timeout=10, response_class=Response):
+	"""Simple fetch of a URI. No request message payload"""
+	return send(Request(), uri, 'GET', timeout, response_class)
 
-class Response(object):
-	"""Response from a download request."""
+def send(message, uri=None, method='GET', timeout=10, response_class=Response):
+	"""Send a Request Message and get a Response Message."""
+	if not uri:
+		uri = message.uri
 
-	def __init__(self):
-		"""Creates a blank response object."""
-		self.uri = ''
-		self.headers = {}
-		self.status = 0
-		self.body = ''
+	outheaders = {
+		'Accept': 'text/html,application/xhtml+xml,' + \
+					'application/xml;q=0.9,*/*;q=0.8',
+		# User agent and protocol
+		'User-Agent': '%s (v%s/+http://github.com/echelon/sylph)' % \
+								(settings.SOFTWARE_NAME,
+								settings.SOFTWARE_VERSION),
+		'X-Sylph-Protocol-Version': settings.PROTOCOL_VERSION,
+	}
 
-	@classmethod
-	def from_data(cls, uri, headers={}, status=0, body=''):
-		"""Create a Response object from the data provided.
-		This type of factory constructor is required since Python
-		does not allow method overloading"""
-		response = cls()
-		response.uri = uri
-		response.headers = headers
-		response.status = status
-		response.body = body
-		return response
+	# Add custom headers and/or overwrite defaults above
+	for hk, hv in message.headers.iteritems():
+		outheaders[hk] = hv
 
-	def get_headers(self):
-		return self.headers
+	post = None
+	if message.post or method.upper() == 'POST':
+		method = 'POST'
+		post = urlencode(messsage.post)
+		outheaders['Content-type'] = 'application/x-www-form-urlencoded'
 
-	def get_uri(self):
-		return self.uri
+	# Use the appropriate message store
+	if response_class:
+		response = response_class(uri)
+	else:
+		response = Response(uri)
 
-	def get_body(self):
-		return self.body
+	try:
+		uri = urlparse(uri)
+		conn = httplib.HTTPConnection(uri.hostname, uri.port, timeout=timeout)
+		if method == 'POST':
+			conn.request(method, uri.path, post, outheaders)
+		else:
+			conn.request(method, uri.path, headers=outheaders)
 
-	def get_status(self):
-		return self.status
+		resp = conn.getresponse()
 
-	def had_fatal_error(self):
-		pass # TODO FIXME
+		# TODO: Handle redirects...
+		# Allow user to select between follow/nofollow, plus max redirect cnt
+
+		for h in resp.getheaders():
+			response.headers[h[0]] = h[1]
+
+		# XXX: Temporary debugging...
+		response.status = resp.status
+		if response.status != 200:
+			from sylph.utils.debug import parse_endpoint_trace
+			print parse_endpoint_trace(resp.read()) # XXX Debug trace
+			raise Exception, "An exception occurred in comms."
+
+		response.body = resp.read()
+		conn.close() # XXX/TODO: In the future, keep this open. 
+
+	except Exception as e:
+		print "CONNECTION ERROR"
+		print e
+		pass
+
+	return response
 
